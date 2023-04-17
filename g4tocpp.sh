da9e7e80852c140882d48e039d7162392dc13506
kylinsoft <36284238+kylinsoft@users.noreply.github.com>
2023-04-17 10:20:09 +0800

    Create g4tocpp.sh

---
+++ g4tocpp.sh
@@ -0,0 +1,2 @@
+antlr4 Sysy.g4 -Dlanguage=Cpp -listener -visitor -o generated/
+#java -cp "/usr/local/lib/antlr-4.9.3-complete.jar:$CLASSPATH" org.antlr.v4.Tool Sysy.g4 -Dlanguage=Cpp -listener -visitor -o generated/
